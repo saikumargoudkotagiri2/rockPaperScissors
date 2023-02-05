f1a61fd2b42d17356ed42623ab9a6a5ec66efedf
saikumar <saikumargoudkotagiri2@gmail.com>
2023-02-05 18:25:35 +0000

    rockPaperScissors

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import RpsPage from './components/RpsPage'
+
+import './App.css'
+
+// eslint-disable-next-line no-unused-vars
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <RpsPage />
+
+export default App
